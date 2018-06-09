0652b0810c5b32a60c3140eb4d0307b3a699ee00
IYehezkelov <itay.yehezkelov@gmail.com>
2018-06-09 19:19:17 +0300

    Nell'Armonia PBCS api scripting with python V0.0.0

--- Lib/funcs.py
+++ Lib/funcs.py
@@ -1,32 +1,38 @@
+# imported python modules #
+
 import requests                                 # python HTTP module
 from requests.auth import HTTPBasicAuth         # generate BasicAuth credentials
 import json                                     # JSON parser
 import pandas as pd                             # data analysis module. used to manipulate files
 import zipfile                                  # ZIP module
-import time                                     # timeout
 
+# user credentials
 user = "a490304.IYehezkelov"
 pw = "A0547780121b"
+
+#####################################################################################################
+###########                                    FUNCTIONS                                  ###########
+#####################################################################################################
+
 
 def url():
     """returns object 'url' which contains all Oracle PBCS URL's"""
 
     url = {
-            "export":"https://planning-test-a490304.pbcs.em2.oraclecloud.com/HyperionPlanning/rest/v3/applications/DELPHI/jobs",
-            "download":"https://planning-test-a490304.pbcs.em2.oraclecloud.com:443/interop/rest/11.1.2.3.600/applicationsnapshots/Scenario_export.zip/contents",
-            "upload":"https://planning-test-a490304.pbcs.em2.oraclecloud.com:443/interop/rest/11.1.2.3.600/applicationsnapshots/Scenario_import.csv/contents?q={'isLast':true,'chunkSize':5522,'isFirst':true}",
-            "import":"https://planning-test-a490304.pbcs.em2.oraclecloud.com/HyperionPlanning/rest/v3/applications/DELPHI/jobs",
-            "delete":"https://planning-test-a490304.pbcs.em2.oraclecloud.com:443/interop/rest/11.1.2.3.600/applicationsnapshots/Scenario_import.csv"
-            }
+            "jobs":"https://planning-test-a490304.pbcs.em2.oraclecloud.com/HyperionPlanning/rest/v3/applications/DELPHI/jobs",
+            "files":"https://planning-test-a490304.pbcs.em2.oraclecloud.com:443/interop/rest/11.1.2.3.600/applicationsnapshots"
+    }
+
     return url
 url = url()
 
+
 def runJob(job_type, job_name):
     """
-    runs a pre-defined job in a PBCS instance.
-    accepts 2 parameters:
-        job_type = PBCS job type
-        job_name = PBCS job name
+        runs a pre-defined job in a PBCS instance.
+        accepts 2 parameters:
+            1) job_type = PBCS job type
+            2) job_name = PBCS job name
     """
 
     # payload & headers
@@ -34,7 +40,7 @@ def runJob(job_type, job_name):
     headers = {"Content-Type":"application/json"}
 
     # send request
-    res = requests.post(url=url['export'], auth=HTTPBasicAuth(user, pw), data=payload, headers=headers)
+    res = requests.post(url=url['jobs'], auth=HTTPBasicAuth(user, pw), data=payload, headers=headers)
     res.raise_for_status()
 
     # print results
@@ -44,42 +50,49 @@ def runJob(job_type, job_name):
     #return reponse object
     return res
 
-def downloadSnapshot():
-    """download a specific application snapshot"""
+
+def downloadFile(pbcs_file, local_file):
+    """
+        download a file from PBCS inbox & unzip it
+        accepts 2 parameters:
+            1) file_dl = name of PBCS inbox file
+            2) file_loc = name of local file
+    """
 
     # send request
-    res = requests.get(url=url['download'], auth=HTTPBasicAuth(user, pw), stream=True)
+    res = requests.get(url=url['files']+'/'+pbcs_file+'/contents', auth=HTTPBasicAuth(user, pw), stream=True)
     res.raise_for_status()
 
     # print results
-    print('LCM file downloaded')
+    print('file downloaded')
     print('request 2 status: ' + str(res.status_code))
 
+    with open('./responses/'+local_file, 'wb') as handle:
+        for chunk in res.iter_content(chunk_size=1024):
+            handle.write(chunk)
+
+    # extract 'scenario.zip'
+    zf = zipfile.ZipFile('./responses/'+local_file)
+    zf.extractall('./responses/content')
     #return reponse object
     return res
 
-def deleteFromInbox():
-    """delete file from PBCS inbox"""
 
-    # send request & error handling
-    res = requests.delete(url=url['delete'],auth=HTTPBasicAuth(user, pw))
-    res.raise_for_status()
+def uploadFile(file_name):
+    """
+        uploads payload file to PBCX inbox
+        accepts 1 param:
+        1) file_name = path of the file to be uploaded
+    """
 
-    # print results
-    print('deleted existing "scenario_import.csv" from PBCS inbox')
-    print('request 3 status: ' + str(res.status_code))
+    payload = open('./responses/content/'+file_name, 'rb').read()
 
-    #return reponse object
-    return res
-
-def uploadSnapshot(payload):
-    """uploads payload file to PBCX inbox"""
-
-    # headers
+    # headers & params
     headers = {'Content-Type':'application/octet-stream'}
+    params = {'isLast':'true','chunkSize':'5522','isFirst':'true'}
 
     # send request & error handling
-    res = requests.post(url=url['upload'], auth=HTTPBasicAuth(user, pw),headers=headers, data=payload)
+    res = requests.post(url=url['files']+'/'+file_name+'/contents', auth=HTTPBasicAuth(user, pw),headers=headers, data=payload, params=params)
     res.raise_for_status()
 
     # print results
@@ -89,19 +102,33 @@ def uploadSnapshot(payload):
     #return reponse object
     return res
 
-def setStartPeriod(file, start_period):
-    """set a new start period for the forecast scenario_import"""
 
-    res = downloadSnapshot()
+def deleteFile(file_name):
+    """
+        delete file from PBCS inbox
+        accepts 1 parameter:
+            1) file_name = file to be deleted from PBCS inbox
+    """
 
-        # save response to 'responses' folder as 'content.zip'
-    with open(file, 'wb') as handle:
-        for chunk in res.iter_content(chunk_size=1024):
-            handle.write(chunk)
+    # send request & error handling
+    res = requests.delete(url=url['files']+'/'+file_name,auth=HTTPBasicAuth(user, pw))
+    res.raise_for_status()
 
-        # extract 'scenario.zip'
-    zf = zipfile.ZipFile(file)
-    zf.extractall('./responses/content')
+    # print results
+    print('deleted existing "scenario_import.csv" from PBCS inbox')
+    print('request 3 status: ' + str(res.status_code))
+
+    #return reponse object
+    return res
+
+
+def setStartPeriod(file_name, start_period):
+    """
+        set a new start period for the forecast scenario_import
+        accepts 2 parameters:
+            1) file_name = name of the file to be manipulated
+            2) start_period = new start period
+    """
 
         # replace start period value with nPer
     df = pd.read_csv('./responses/content/IYehezkelov_ExportedMetadata_Scenario.csv',encoding='utf-8')
---
+++ script.py
@@ -0,0 +1,52 @@
+from Lib.funcs import *
+
+###
+### step 1 - trigger LCM export job
+###
+
+# parameters
+jobType = "Export Metadata"
+jobName = "Scenario_Export"
+
+runJob(jobType, jobName)
+
+###
+### step 2 - Download snapshot, save & unzip it
+###
+
+# parameters
+pbcsFile = 'Scenario_export.zip'
+savedFile = 'Scenario.zip'
+
+downloadFile(pbcsFile, savedFile)
+
+# set a new forecast start period
+setStartPeriod(savedFile, 'May')
+
+###
+### step 3 - delete file from PBCS
+###
+
+# parameters
+file = 'scenario_import.csv'
+
+deleteFile(file)
+
+###
+### step 4 - upload new file to PBCS
+###
+
+# parameters
+file = 'Scenario_import.csv'
+
+uploadFile(file)
+
+###
+### step 5 - trigger LCM import job
+###
+
+# parameters
+jobType = "Import Metadata"
+jobName = "Scenario_import"
+
+runJob(jobType, jobName)
