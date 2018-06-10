c016c1545295cf6c51e2a8d4a913cb6de261fc78
IYehezkelov <itay.yehezkelov@gmail.com>
2018-06-10 11:54:43 +0300

    fixed minor typo in 'base' added 'app_name' to url building blocks

--- Lib/url_composer.py
+++ Lib/url_composer.py
@@ -1,11 +1,12 @@
 def url():
     """returns object 'url' which contains all Oracle PBCS URL's"""
 
+    # url building blocks
     domain = '-'+'a490304'
     env = '-'+'test'
+    app_name = 'DELPHI'
 
-    base = "https://planning"+env+".pbcs.em2.oraclecloud.com"
-
+    base = "https://planning"+env+domain+".pbcs.em2.oraclecloud.com"
     pln_ver = requests.get(url = 'https://planning-test-a490304.pbcs.em2.oraclecloud.com/HyperionPlanning/rest/', auth=HTTPBasicAuth(user, pw)) # get planning api version
     lcm_ver = requests.get(url = 'https://planning-test-a490304.pbcs.em2.oraclecloud.com:443/interop/rest/', auth=HTTPBasicAuth(user, pw)) # get lcm api version
 
