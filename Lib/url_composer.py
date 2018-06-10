510ecd71767ea3fa65af3be66dfb141fef449a1d
IYehezkelov <itay.yehezkelov@gmail.com>
2018-06-10 11:57:37 +0300

    minor changes after merge issue

--- Lib/url_composer.py
+++ Lib/url_composer.py
@@ -1,19 +1,19 @@
 def url():
     """returns object 'url' which contains all Oracle PBCS URL's"""
 
-    # url building blocks
-    domain = '-'+'a490304'
-    env = '-'+'test'
-<<<<<<< HEAD
-    app_name = 'DELPHI'
-
-=======
->>>>>>> aa8ab5b429e73b40f6f062b575235cc09bb37738
-    base = "https://planning"+env+domain+".pbcs.em2.oraclecloud.com"
+    # get API versions
     pln_ver = requests.get(url = 'https://planning-test-a490304.pbcs.em2.oraclecloud.com/HyperionPlanning/rest/', auth=HTTPBasicAuth(user, pw)) # get planning api version
     lcm_ver = requests.get(url = 'https://planning-test-a490304.pbcs.em2.oraclecloud.com:443/interop/rest/', auth=HTTPBasicAuth(user, pw)) # get lcm api version
 
+    # url building blocks
+    domain = '-'+'a490304'
+    env = '-'+'test'
+    app_name = 'DELPHI'
+    base = "https://planning"+env+domain+".pbcs.em2.oraclecloud.com"
+    
+    # planning URL
     planning = base+'/'+'HyperionPlanning/rest/'+pln_ver
+    # migration URL
     lcm = base+':'+'443/interop/rest/'+lcm_ver
 
     url = {
