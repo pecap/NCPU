76f41a8aee9aef8ebc7368db936af185bdce9603
IYehezkelov <itay.yehezkelov@gmail.com>
2018-06-11 10:12:51 +0300

    added 'get API version' http requests to url_composer.py tested - url's are now composed properly

--- Lib/url_composer.py
+++ Lib/url_composer.py
@@ -1,24 +1,38 @@
+import requests                                 # python HTTP module
+from requests.auth import HTTPBasicAuth         # generate BasicAuth credentials
+import json                                     # JSON parser
+
 def url():
     """returns object 'url' which contains all Oracle PBCS URL's"""
 
-    # get API versions
-    pln_ver = requests.get(url = 'https://planning-test-domain.pbcs.em2.oraclecloud.com/HyperionPlanning/rest/', auth=HTTPBasicAuth(user, pw)) # get planning api version
-    lcm_ver = requests.get(url = 'https://planning-test-domain.pbcs.em2.oraclecloud.com:443/interop/rest/', auth=HTTPBasicAuth(user, pw)) # get lcm api version
-
     # url building blocks
-    domain = '-'+'PBCS_DOMAIN'
-    env = '-'+'test'
-    app_name = 'PBCS_APP_NAME'
+    domain = '-'+'domain_name'
+    env = '-'+'env_type'
+    app_name = 'app_name'
     base = "https://planning"+env+domain+".pbcs.em2.oraclecloud.com"
 
-    # planning URL
-    planning = base+'/'+'HyperionPlanning/rest/'+pln_ver
-    # migration URL
-    lcm = base+':'+'443/interop/rest/'+lcm_ver
+    # credentials to get versions
+    user = domain+"."+"user_name"
+    pw = "pw"
 
-    url = {
-            "jobs":"https://planning-test-domain.pbcs.em2.oraclecloud.com/HyperionPlanning/rest/v3/applications/app_name/jobs",
-            "files":"https://planning-test-domain.pbcs.em2.oraclecloud.com:443/interop/rest/11.1.2.3.600/applicationsnapshots"
-    }
+    # get API versions
+    getPlnVersion = requests.get(url = base+'/'+'HyperionPlanning/rest/', auth=HTTPBasicAuth(user, pw)) # get planning api version
+
+    pVer = json.loads(getPlnVersion.text)
+    items = pVer['items']
+
+    for i in items:
+        if i['isLatest'] == True:
+            pln_ver = i['version']
+
+    getLcmVersion = requests.get(url = base+':'+'443/interop/rest/', auth=HTTPBasicAuth(user, pw)) # get lcm api version
+
+    lVer = json.loads(getLcmVersion.text)
+    lcm_ver = lVer['items'][0]['version']
+
+    # planning URL
+    planning_url = base+'/'+'HyperionPlanning/rest/'+pln_ver
+    # migration URL
+    lcm_url = base+':'+'443/interop/rest/'+lcm_ver
 
     return url
