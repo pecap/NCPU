b0870bc1aa51d13c917fb605faacd03d472f36bb
IYehezkelov <itay.yehezkelov@gmail.com>
2018-06-11 10:22:41 +0300

    updated naming conventions in url_composer.py

--- Lib/url_composer.py
+++ Lib/url_composer.py
@@ -5,34 +5,42 @@ import json                                     # JSON parser
 def url():
     """returns object 'url' which contains all Oracle PBCS URL's"""
 
-    # url building blocks
-    domain = '-'+'domain_name'
-    env = '-'+'env_type'
-    app_name = 'app_name'
-    base = "https://planning"+env+domain+".pbcs.em2.oraclecloud.com"
-
     # credentials to get versions
     user = domain+"."+"user_name"
     pw = "pw"
 
-    # get API versions
-    getPlnVersion = requests.get(url = base+'/'+'HyperionPlanning/rest/', auth=HTTPBasicAuth(user, pw)) # get planning api version
+    # url building blocks
+    domain = '-'+'domain_name'
+    env = '-'+'env_type'
+    app_name = 'app_name'
 
-    pVer = json.loads(getPlnVersion.text)
-    items = pVer['items']
+    # base url
+    base = "https://planning"+env+domain+".pbcs.em2.oraclecloud.com"
+
+    # get API versions
+    res = requests.get(url = base+'/'+'HyperionPlanning/rest/', auth=HTTPBasicAuth(user, pw)) # get planning api version
+
+    res_content = json.loads(getPlnVersion.text)
+    items = res_content['items']
 
     for i in items:
         if i['isLatest'] == True:
             pln_ver = i['version']
 
-    getLcmVersion = requests.get(url = base+':'+'443/interop/rest/', auth=HTTPBasicAuth(user, pw)) # get lcm api version
+    res = requests.get(url = base+':'+'443/interop/rest/', auth=HTTPBasicAuth(user, pw)) # get lcm api version
 
-    lVer = json.loads(getLcmVersion.text)
-    lcm_ver = lVer['items'][0]['version']
+    res_content = json.loads(getLcmVersion.text)
+    lcm_ver = res_content['items'][0]['version']
 
     # planning URL
     planning_url = base+'/'+'HyperionPlanning/rest/'+pln_ver
     # migration URL
     lcm_url = base+':'+'443/interop/rest/'+lcm_ver
+
+    url =
+    {
+        'jobs':planning_url,
+        'files':lcm_url
+    }
 
     return url
